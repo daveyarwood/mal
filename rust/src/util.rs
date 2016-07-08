02b488a6afdca8c13bfa55e56c71635330b37df5
Dave Yarwood <dave.yarwood@gmail.com>
2016-07-08 08:03:50 -0400

    steal solution from kanaka/mal :P

--- rust/src/util.rs
+++ rust/src/util.rs
@@ -1,15 +1,21 @@
 use regex::Regex;
 
 pub fn escape(string: &str) -> String {
-    // " => \"
-    let step1 = Regex::new(r#"""#).unwrap().replace_all(string, "\\\"");
-    // (newline) => \n
-    let step2 = Regex::new(r"\n").unwrap().replace_all(&step1, "\\n");
-    // \ => \\
-    // let step3 = Regex::new(r"\\").unwrap().replace_all(&step2, "\\\\");
+    let mut escaped = String::new();
+    for c in string.chars() {
+        let _ = match c {
+            '"' => escaped.push_str("\\\""),
+            '\\' => escaped.push_str("\\\\"),
+            '\x08' => escaped.push_str("\\b"),
+            '\x0c' => escaped.push_str("\\f"),
+            '\n' => escaped.push_str("\\n"),
+            '\r' => escaped.push_str("\\r"),
+            '\t' => escaped.push_str("\\t"),
+            _ => escaped.push(c),
+        };
+    };
 
-    step2
-    // step3
+    escaped
 }
 
 pub fn unescape(string: &str) -> String {
